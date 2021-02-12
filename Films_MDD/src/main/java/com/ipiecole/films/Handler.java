278afbf614c36fdd10426bc2d7f999923840e01a
Mathis OLLIER <maol.devpro@gmail.com>
2021-02-12 15:50:30 +0100

    end tp

--- Films_MDD/src/main/java/com/ipiecole/films/Handler.java
+++ Films_MDD/src/main/java/com/ipiecole/films/Handler.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class Handler implements RequestHandler<Object, GatewayResponse> {
 
